170a32876cecc6fe51fa642f4435b06a1d266053
Shanmugapriya <priyajanarthanan@gmail.com>
2025-04-28 08:50:25 -0500

    Added ouptut

--- task-manager/app.js
+++ task-manager/app.js
@@ -15,10 +15,27 @@ tasks = fileHdler.loadTasks('tasks.json');
 console.log(tasks);
 
 //Add new tasks to the list.
-let newtasks = taskMgr.addTask(tasks,'Pack new clothes');
+let newtasks = taskMgr.addTask(tasks,'Cut the vegetables');
 
 //List all tasks.
 taskMgr.listTasks(newtasks);
 
 //Save the updated task list back to the file.
 fileHdler.saveTasks('tasks.json',newtasks); 
+
+/**
+ *  Output
+ * All tasks loaded !
+{
+  'Complete Assignment': 'Complete Assignment',
+  'Do the laundry': 'Do the laundry',
+  'Cook food': 'Cook food',
+  'Do the dishes': 'Do the dishes',
+  'Pack new clothes': 'Pack new clothes'
+}
+New task successfully added!
+Listing all the tasks...
+{"Complete Assignment":"Complete Assignment","Do the laundry":"Do the laundry","Cook food":"Cook food","Do the dishes":"Do the dishes","Pack new clothes":"Pack new clothes","Cut the vegetables":"Cut the vegetables"}
+Tasks are updated in the file!
+ * 
+ */
